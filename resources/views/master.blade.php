b9d7e51c50d5349ef0ab05b289614ef136902ba5
Hillel Coren <hillelcoren@gmail.com>
2018-07-13 12:28:40 +0300

    Updates

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -13,14 +13,14 @@
         </script>
     @endif
 
+    <title>@yield('title') | {{ config('app.name') }}</title>
+
     <meta charset="utf-8">
     <meta id="token" name="token" value="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="@yield('description')">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
     <link rel="shortcut icon" href="{{ asset('/images/favicon.png') }}">
-
-    <title>@yield('title') | {{ config('app.name') }}</title>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
     <script defer src="https://use.fontawesome.com/releases/v5.1.0/js/all.js"></script>
