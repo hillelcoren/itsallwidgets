991706935ee2ef128e9b0922376193e2e4d4163b
Hillel Coren <hillelcoren@gmail.com>
2018-07-15 13:38:48 +0300

    Added RSS

--- config/feed.php
+++ config/feed.php
@@ -1,29 +1,18 @@
 <?php
 
 return [
+
     'feeds' => [
+
         'main' => [
-            /*
-             * Here you can specify which class and method will return
-             * the items that should appear in the feed. For example:
-             * 'App\Model@getAllFeedItems'
-             *
-             * You can also pass an argument to that method:
-             * ['App\Model@getAllFeedItems', 'argument']
-             */
+
             'items' => 'App\Models\FlutterApp@getFeedItems',
-
-            /*
-             * The feed will be available on this url.
-             */
             'url' => 'feed',
-
             'title' => 'It\'s All Widgets!',
-
-            /*
-             * The view that will render the feed.
-             */
             'view' => 'feed::feed',
+
         ],
+
     ],
+
 ];
