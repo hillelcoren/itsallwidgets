904047c7b357fbffacd18d6386bb889cb30a0a7c
Hillel Coren <hillelcoren@gmail.com>
2018-07-18 16:23:14 +0300

    Updates

--- app/Http/Controllers/FlutterAppController.php
+++ app/Http/Controllers/FlutterAppController.php
@@ -152,7 +152,7 @@ class FlutterAppController extends Controller
 
     public function sitemap()
     {
-        $str = '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+        $str = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">";
         $str .= '<url><loc>' . config('app.url') . '</loc><lastmod>' . date('Y-m-d') . '</lastmod><changefreq>daily</changefreq><priority>1</priority></url>';
 
         $apps = cache('flutter-app-list');
