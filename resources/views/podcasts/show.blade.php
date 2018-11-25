89c8109af6c205d7516fb9fb494813d9ecdd914e
Hillel Coren <hillelcoren@gmail.com>
2018-11-25 16:02:13 +0200

    Podcast

---
+++ resources/views/podcasts/show.blade.php
@@ -0,0 +1,15 @@
+@extends('master')
+
+@section('title', $episode->guest)
+@section('description', $episode->short_description)
+
+@section('content')
+
+
+	<section class="section is-body-font">
+		<div class="container">
+
+		</div>
+	</section>
+
+@stop
