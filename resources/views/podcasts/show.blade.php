37a141188c8e86256afa18ecb71350e6cd9c0def
Hillel Coren <hillelcoren@gmail.com>
2018-11-25 20:58:38 +0200

    Podcast

--- resources/views/podcasts/show.blade.php
+++ resources/views/podcasts/show.blade.php
@@ -1,7 +1,13 @@
 @extends('master')
 
-@section('title', $episode->guest)
+@section('title', $episode->title)
 @section('description', $episode->short_description)
+
+@section('header_title', 'An open podcast for Flutter developers')
+@section('header_subtitle', 'Share your Flutter story with the community')
+@section('header_button_url', url(auth()->check() ? 'podcast/submit' : 'auth/google?intended_url=podcast/submit'))
+@section('header_button_label', 'REQUEST INTERVIEW')
+@section('header_button_icon', 'fas fa-microphone')
 
 @section('content')
 
