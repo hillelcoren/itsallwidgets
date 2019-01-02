903a8a049abb78b5f93ef18e01023c9cb3b94db6
Hillel Coren <hillelcoren@gmail.com>
2019-01-02 12:36:31 +0200

    Update terms

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -25,59 +25,63 @@
 
                         <p>
                             If You are using It’s All Widgets on behalf of a developer, company, or organization, by use of It’s All Widgets You represent and warrant that You are a legally authorized representative with the authority to bind to these terms. All third parties shall be referred to as “You” for the remaining terms of use.
-                        <p><br/>
+                        </p><br/>
 
                         <p>
                             By browsing or submitting an app to It’s All Widgets, You acknowledge that You have read, understood, and agree the following legally binding terms:
-                        <p><br/>
+                        </p><br/>
 
                         <p>
                             It’s All Widgets is a digital showcase/ open list of third-party apps built with Flutter. It’s All Widgets allows independent third-party developers to submit apps independently.
-                        <p><br/>
+                        </p><br/>
+
+                        <p>
+                            The Flutter team has full discretion to mention apps submitted on It’s All Widgets.com in marketing and promotion, or external communications.
+                        </p><br/>
 
                         <p>
                             The Apps offered on the It’s All Widgets are made available by third party developers that are not affiliated with It’s All Widgets, as such, You agree that It’s All Widgets is not to be held responsible for such apps terms of use and privacy. Each app submitted by a third party is subject to that individual developer/parties’ terms of use and privacy policy concluded directly with each app developer & app user.
-                        <p><br/>
+                        </p><br/>
 
                         <p>
                             You may not attempt to represent It’s All Widgets in any capacity to resell, sublicense, redistribute apps in any capacity.
-                        <p><br/>
+                        </p><br/>
 
                         <p>
                             You own all data submitted for showcase on It's All Widgets. You may not post, upload, or distribute within apps any content that is defamatory, libellous, inaccurate, pornographic, harassing, threatening, hateful, ethnically offensive, or otherwise inappropriate unlawful (including in violation of any intellectual property or similar laws).
-                        <p><br/>
+                        </p><br/>
 
                         <p>
                             It’s All Widgets will make every effort to ensure that the showcase remains online and provide uninterrupted service to the showcase, however use of the It’s All Widgets is not a data storage agreement and It’s All Widgets shall not be held responsible for the ongoing storage or potential loss of data in any form or capacity.
-                        <p><br/>
+                        </p><br/>
 
                         <p>
                             You may not misrepresent the source of an app, impersonate an app developer, falsely claim an affiliation with any app developer, or perform any other similar fraudulent activity that would affect a showcased app developer.
-                        <p><br/>
+                        </p><br/>
 
                         <p>
                             You may not connect It’s All Widgets to any third-party platform without written consent from It’s All Widgets.
-                        <p><br/>
+                        </p><br/>
 
                         <p>
                             You are solely responsible for Your app submissions and the consequences of those submissions. You affirm, represent, and warrant that: (1) You are the creator and owner of, or have all the necessary licenses, rights, consents, and permissions to authorize It’s All Widgets to showcase your app; (2) Your submissions do not: (a) infringe, violate, or misappropriate any third party right, including any copyright, trademark, patent, trade secret, moral right, privacy right, right of publicity, or any other intellectual property or proprietary right or (b) slander, defame, libel, or invade the right of privacy of any person; and (3) Your submission does contain any viruses, adware, spyware, worms, or other malicious code or any content or file that may have the effect of compromising any security or access control feature of the It’s All Widgets showcase. Violators of third-party rights may be subject to criminal and civil liability.
-                        <p><br/>
+                        </p><br/>
 
                         <p>
-                            It’s All Widgets reserves the right to determine in its sole discretion whether to remove any submission or other content without penalty. It’s All Widgets will not be liable to You or any third party for any such termination. Any suspected fraudulent, abusive, or illegal activity may be referred to appropriate law enforcement authorities. These remedies are in addition to any other remedies It’s All Widgets may have at law or in equity. 
-                        <p><br/>
+                            It’s All Widgets reserves the right to determine in its sole discretion whether to remove any submission or other content without penalty. It’s All Widgets will not be liable to You or any third party for any such termination. Any suspected fraudulent, abusive, or illegal activity may be referred to appropriate law enforcement authorities. These remedies are in addition to any other remedies It’s All Widgets may have at law or in equity.
+                        </p><br/>
 
                         <p>
                             Indemnification.  You agree to indemnify, defend, and hold It’s All Widgets, its affiliated companies, contractors, employees, agents and its third-party suppliers, licensors, and partners harmless from any claims, losses, damages, liabilities, including legal fees and expenses, arising out of Your use or misuse of the It’s All Widgets showcase. 
-                        <p><br/>
+                        </p><br/>
 
                         <p>
                             Your expressly agree that the use of It’s All Widgets is at your sole risk. Under no circumstance will It’s All Widgets or its associates be liable for any special, direct or indirect, incidental, consequential, punitive, or exemplary damages by Your use of It’s All Widgets in any capacity.
-                        <p><br/>
+                        </p><br/>
 
                         <p>
                             If You have a dispute with a User or Developer, You release It’s All Widgets from claims, demands and damages of every kind and nature, known and unknown, arising out of or in any way connected with such disputes.
-                        <p><br/>
+                        </p><br/>
 
                     </div><br/>
                 </div>
