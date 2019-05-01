c9d728b50a4382d2351f7e8ecb46d944563d4113
Hillel Coren <hillelcoren@gmail.com>
2019-05-01 09:13:09 +0300

    Reddit link

--- app/Models/PodcastEpisode.php
+++ app/Models/PodcastEpisode.php
@@ -63,7 +63,7 @@ class PodcastEpisode extends Model implements Feedable
 
     public function toFeedItem()
     {
-        $summary = htmlentities(strip_tags($this->short_description . "\n" . $this->long_description));
+        $summary = htmlspecialchars(strip_tags($this->short_description . "\n" . $this->long_description));
 
         if ($this->reddit_url) {
             $summary .= "\n\n" . $this->reddit_url;
