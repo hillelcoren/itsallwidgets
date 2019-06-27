93ffdb04a85d8a948eb9b53da622cda97ac13dc2
Hillel Coren <hillelcoren@gmail.com>
2019-06-27 13:11:36 +0300

    Flutter X

--- app/Console/Commands/LoadArtifacts.php
+++ app/Console/Commands/LoadArtifacts.php
@@ -111,8 +111,6 @@ class LoadArtifacts extends Command
                         }
 
                         $artifact->save();
-
-                        exit;
                     }
                 }
             }
