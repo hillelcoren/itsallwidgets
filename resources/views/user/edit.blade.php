9ee8701eeb367f6a313d293cdfec1f7475f1138e
Hillel Coren <hillelcoren@gmail.com>
2019-09-03 18:33:06 +0300

    profiles

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -73,26 +73,24 @@
 						</div>
 					</div>
 
-					@if ($user->is_pro)
-						<div class="field">
-							<label class="label" for="handle">
-								Handle <span class="required">*</span>
-							</label>
-							<div class="control has-icons-left">
-								{{ Form::text('handle', $user->handle, ['class' => 'input', 'required' => true]) }}
+					<div class="field">
+						<label class="label" for="handle">
+							Handle <span class="required">*</span>
+						</label>
+						<div class="control has-icons-left">
+							{{ Form::text('handle', $user->handle, ['class' => 'input', 'required' => true]) }}
 
-								<span class="icon is-small is-left">
-									<i class="fas fa-at"></i>
+							<span class="icon is-small is-left">
+								<i class="fas fa-at"></i>
+							</span>
+
+							@if ($errors->has('handle'))
+								<span class="help is-danger">
+									{{ $errors->first('handle') }}
 								</span>
-
-								@if ($errors->has('handle'))
-									<span class="help is-danger">
-										{{ $errors->first('handle') }}
-									</span>
-								@endif
-							</div>
+							@endif
 						</div>
-					@endif
+					</div>
 
 				</div>
 
@@ -101,162 +99,159 @@
 			<p>&nbsp;</p>
 			<p>&nbsp;</p>
 
-			@if ($user->is_pro)
-				<div class="subtitle">Optional Fields</div>
+			<div class="subtitle">Optional Fields</div>
 
-				<article class="message is-dark is-elevated">
-					<div class="message-body">
+			<article class="message is-dark is-elevated">
+				<div class="message-body">
 
-						<div class="field">
-							<label class="label" for="bio">
-								Bio
-							</label>
-							<div class="control">
+					<div class="field">
+						<label class="label" for="bio">
+							Bio
+						</label>
+						<div class="control">
 
-								{{ Form::textarea('bio', $user->bio, ['class' => 'textarea', 'rows' => 3]) }}
+							{{ Form::textarea('bio', $user->bio, ['class' => 'textarea', 'rows' => 3]) }}
 
-								@if ($errors->has('bio'))
-									<span class="help is-danger">
-										{{ $errors->first('bio') }}
-									</span>
-								@endif
-							</div>
-						</div>
-
-						<div class="field">
-							<label class="label" for="website_url">
-								Website
-							</label>
-							<div class="control has-icons-left">
-
-								{{ Form::url('website_url', $user->website_url, ['class' => 'input', 'placeholder' => 'https://example.com']) }}
-
-								<span class="icon is-small is-left">
-									<i class="fas fa-globe"></i>
+							@if ($errors->has('bio'))
+								<span class="help is-danger">
+									{{ $errors->first('bio') }}
 								</span>
-
-								@if ($errors->has('website_url'))
-									<span class="help is-danger">
-										{{ $errors->first('website_url') }}
-									</span>
-								@endif
-							</div>
+							@endif
 						</div>
-
-
-
-						<div class="field">
-							<label class="label" for="github_url">
-								6itHub
-							</label>
-							<div class="control has-icons-left">
-
-								{{ Form::url('github_url', $user->github_url, ['class' => 'input', 'placeholder' => 'https://github.com']) }}
-
-								<span class="icon is-small is-left">
-									<i class="fab fa-github"></i>
-								</span>
-
-								@if ($errors->has('github_url'))
-									<span class="help is-danger">
-										{{ $errors->first('github_url') }}
-									</span>
-								@endif
-							</div>
-						</div>
-
-
-						<div class="field">
-							<label class="label" for="medium_url">
-								Medium
-							</label>
-							<div class="control has-icons-left">
-
-								{{ Form::url('medium_url', $user->medium_url, ['class' => 'input', 'placeholder' => 'https://medium.com']) }}
-
-								<span class="icon is-small is-left">
-									<i class="fab fa-medium"></i>
-								</span>
-
-								@if ($errors->has('medium_url'))
-									<span class="help is-danger">
-										{{ $errors->first('medium_url') }}
-									</span>
-								@endif
-							</div>
-						</div>
-
-
-						<div class="field">
-							<label class="label" for="twitter_url">
-								Twitter
-							</label>
-							<div class="control has-icons-left">
-
-								{{ Form::url('twitter_url', $user->twitter_url, ['class' => 'input', 'placeholder' => 'https://twitter.com']) }}
-
-								<span class="icon is-small is-left">
-									<i class="fab fa-twitter"></i>
-								</span>
-
-								@if ($errors->has('twitter_url'))
-									<span class="help is-danger">
-										{{ $errors->first('twitter_url') }}
-									</span>
-								@endif
-							</div>
-						</div>
-
-
-						<div class="field">
-							<label class="label" for="youtube_url">
-								YouTube
-							</label>
-							<div class="control has-icons-left">
-
-								{{ Form::url('youtube_url', $user->youtube_url, ['class' => 'input', 'placeholder' => 'https://youtube.com']) }}
-
-								<span class="icon is-small is-left">
-									<i class="fab fa-youtube"></i>
-								</span>
-
-								@if ($errors->has('youtube_url'))
-									<span class="help is-danger">
-										{{ $errors->first('youtube_url') }}
-									</span>
-								@endif
-							</div>
-						</div>
-
-
-						<div class="field">
-							<label class="label" for="linkedin_url">
-								LinkedIn
-							</label>
-							<div class="control has-icons-left">
-
-								{{ Form::url('linkedin_url', $user->linkedin_url, ['class' => 'input', 'placeholder' => 'https://linkedin.com']) }}
-
-								<span class="icon is-small is-left">
-									<i class="fab fa-linkedin"></i>
-								</span>
-
-								@if ($errors->has('linkedin_url'))
-									<span class="help is-danger">
-										{{ $errors->first('linkedin_url') }}
-									</span>
-								@endif
-							</div>
-						</div>
-
-
 					</div>
-				</article>
 
-				<p>&nbsp;</p>
-				<p>&nbsp;</p>
-			@endif
+					<div class="field">
+						<label class="label" for="website_url">
+							Website
+						</label>
+						<div class="control has-icons-left">
 
+							{{ Form::url('website_url', $user->website_url, ['class' => 'input', 'placeholder' => 'https://example.com']) }}
+
+							<span class="icon is-small is-left">
+								<i class="fas fa-globe"></i>
+							</span>
+
+							@if ($errors->has('website_url'))
+								<span class="help is-danger">
+									{{ $errors->first('website_url') }}
+								</span>
+							@endif
+						</div>
+					</div>
+
+
+
+					<div class="field">
+						<label class="label" for="github_url">
+							6itHub
+						</label>
+						<div class="control has-icons-left">
+
+							{{ Form::url('github_url', $user->github_url, ['class' => 'input', 'placeholder' => 'https://github.com']) }}
+
+							<span class="icon is-small is-left">
+								<i class="fab fa-github"></i>
+							</span>
+
+							@if ($errors->has('github_url'))
+								<span class="help is-danger">
+									{{ $errors->first('github_url') }}
+								</span>
+							@endif
+						</div>
+					</div>
+
+
+					<div class="field">
+						<label class="label" for="medium_url">
+							Medium
+						</label>
+						<div class="control has-icons-left">
+
+							{{ Form::url('medium_url', $user->medium_url, ['class' => 'input', 'placeholder' => 'https://medium.com']) }}
+
+							<span class="icon is-small is-left">
+								<i class="fab fa-medium"></i>
+							</span>
+
+							@if ($errors->has('medium_url'))
+								<span class="help is-danger">
+									{{ $errors->first('medium_url') }}
+								</span>
+							@endif
+						</div>
+					</div>
+
+
+					<div class="field">
+						<label class="label" for="twitter_url">
+							Twitter
+						</label>
+						<div class="control has-icons-left">
+
+							{{ Form::url('twitter_url', $user->twitter_url, ['class' => 'input', 'placeholder' => 'https://twitter.com']) }}
+
+							<span class="icon is-small is-left">
+								<i class="fab fa-twitter"></i>
+							</span>
+
+							@if ($errors->has('twitter_url'))
+								<span class="help is-danger">
+									{{ $errors->first('twitter_url') }}
+								</span>
+							@endif
+						</div>
+					</div>
+
+
+					<div class="field">
+						<label class="label" for="youtube_url">
+							YouTube
+						</label>
+						<div class="control has-icons-left">
+
+							{{ Form::url('youtube_url', $user->youtube_url, ['class' => 'input', 'placeholder' => 'https://youtube.com']) }}
+
+							<span class="icon is-small is-left">
+								<i class="fab fa-youtube"></i>
+							</span>
+
+							@if ($errors->has('youtube_url'))
+								<span class="help is-danger">
+									{{ $errors->first('youtube_url') }}
+								</span>
+							@endif
+						</div>
+					</div>
+
+
+					<div class="field">
+						<label class="label" for="linkedin_url">
+							LinkedIn
+						</label>
+						<div class="control has-icons-left">
+
+							{{ Form::url('linkedin_url', $user->linkedin_url, ['class' => 'input', 'placeholder' => 'https://linkedin.com']) }}
+
+							<span class="icon is-small is-left">
+								<i class="fab fa-linkedin"></i>
+							</span>
+
+							@if ($errors->has('linkedin_url'))
+								<span class="help is-danger">
+									{{ $errors->first('linkedin_url') }}
+								</span>
+							@endif
+						</div>
+					</div>
+
+
+				</div>
+			</article>
+
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
 			<p>&nbsp;</p>
 			<p>&nbsp;</p>
 
