6831edfbfc32cfb20e10099b4be5baa10622ac3d
Hillel Coren <hillelcoren@gmail.com>
2019-09-03 13:44:05 +0300

    profiles

--- database/migrations/2019_06_28_114928_create_user_activities.php
+++ database/migrations/2019_06_28_114928_create_user_activities.php
@@ -26,6 +26,19 @@ class CreateUserActivities extends Migration
         Schema::table('user_activities', function(Blueprint $table) {
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
+
+        Schema::table('users', function(Blueprint $table) {
+            $table->string('handle')->nullable()->unique();
+            $table->text('bio')->nullable();
+            $table->boolean('is_pro')->default(false);
+            $table->string('profile_url')->default('');
+            $table->string('website_url')->default('');
+            $table->string('github_url')->default('');
+            $table->string('youtube_url')->default('');
+            $table->string('twitter_url')->default('');
+            $table->string('medium_url')->default('');
+            $table->string('linkedin_url')->default('');
+        });
     }
 
     /**
@@ -35,6 +48,19 @@ class CreateUserActivities extends Migration
      */
     public function down()
     {
-        Schema::drop('user_artifacts');
+        Schema::drop('user_activities');
+
+        Schema::table('users', function(Blueprint $table) {
+            $table->dropColumn('handle');
+            $table->dropColumn('bio');
+            $table->dropColumn('is_pro');
+            $table->dropColumn('profile_url');
+            $table->dropColumn('website_url');
+            $table->dropColumn('github_url');
+            $table->dropColumn('youtube_url');
+            $table->dropColumn('twitter_url');
+            $table->dropColumn('medium_url');
+            $table->dropColumn('linkedin_url');
+        });
     }
 }
--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -7,9 +7,9 @@
 @section('content')
 
 	<script>
-		function onFormSubmit() {
-			$('#saveButton').addClass('is-loading').prop('disabled', true);
-		}
+	function onFormSubmit() {
+		$('#saveButton').addClass('is-loading').prop('disabled', true);
+	}
 	</script>
 
 	<p>&nbsp;</p>
@@ -35,12 +35,16 @@
 			<article class="message is-dark is-elevated">
 				<div class="message-body">
 
-                    <div class="field">
-						<label class="label" for="title">
+					<div class="field">
+						<label class="label" for="name">
 							Name <span class="required">*</span>
 						</label>
-						<div class="control">
+						<div class="control has-icons-left">
 							{{ Form::text('name', $user->name, ['class' => 'input', 'required' => true]) }}
+
+							<span class="icon is-small is-left">
+								<i class="fas fa-user"></i>
+							</span>
 
 							@if ($errors->has('name'))
 								<span class="help is-danger">
@@ -50,12 +54,16 @@
 						</div>
 					</div>
 
-                    <div class="field">
-						<label class="label" for="title">
+					<div class="field">
+						<label class="label" for="email">
 							Email <span class="required">*</span>
 						</label>
-						<div class="control">
+						<div class="control has-icons-left">
 							{{ Form::text('name', $user->email, ['class' => 'input', 'required' => true]) }}
+
+							<span class="icon is-small is-left">
+								<i class="fas fa-envelope"></i>
+							</span>
 
 							@if ($errors->has('email'))
 								<span class="help is-danger">
@@ -65,9 +73,186 @@
 						</div>
 					</div>
 
+					<div class="field">
+						<label class="label" for="handle">
+							Handle <span class="required">*</span>
+						</label>
+						<div class="control has-icons-left">
+							{{ Form::text('handle', $user->handle, ['class' => 'input', 'required' => true]) }}
+
+							<span class="icon is-small is-left">
+								<i class="fas fa-at"></i>
+							</span>
+
+							@if ($errors->has('handle'))
+								<span class="help is-danger">
+									{{ $errors->first('handle') }}
+								</span>
+							@endif
+						</div>
+					</div>
+
 				</div>
 
 			</article>
+
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+
+			<div class="subtitle">Optional Fields</div>
+
+			<article class="message is-dark is-elevated">
+				<div class="message-body">
+
+					<div class="field">
+						<label class="label" for="bio">
+							Bio
+						</label>
+						<div class="control">
+
+							{{ Form::textarea('bio', $user->bio, ['class' => 'textarea', 'rows' => 3]) }}
+
+							@if ($errors->has('bio'))
+								<span class="help is-danger">
+									{{ $errors->first('bio') }}
+								</span>
+							@endif
+						</div>
+					</div>
+
+					<div class="field">
+						<label class="label" for="website_url">
+							Website
+						</label>
+						<div class="control has-icons-left">
+
+							{{ Form::url('website_url', $user->website_url, ['class' => 'input', 'placeholder' => 'https://example.com']) }}
+
+							<span class="icon is-small is-left">
+								<i class="fas fa-globe"></i>
+							</span>
+
+							@if ($errors->has('website_url'))
+								<span class="help is-danger">
+									{{ $errors->first('website_url') }}
+								</span>
+							@endif
+						</div>
+					</div>
+
+
+
+					<div class="field">
+						<label class="label" for="github_url">
+							6itHub
+						</label>
+						<div class="control has-icons-left">
+
+							{{ Form::url('github_url', $user->github_url, ['class' => 'input', 'placeholder' => 'https://github.com']) }}
+
+							<span class="icon is-small is-left">
+								<i class="fab fa-github"></i>
+							</span>
+
+							@if ($errors->has('github_url'))
+								<span class="help is-danger">
+									{{ $errors->first('github_url') }}
+								</span>
+							@endif
+						</div>
+					</div>
+
+
+					<div class="field">
+						<label class="label" for="medium_url">
+							Medium
+						</label>
+						<div class="control has-icons-left">
+
+							{{ Form::url('medium_url', $user->medium_url, ['class' => 'input', 'placeholder' => 'https://medium.com']) }}
+
+							<span class="icon is-small is-left">
+								<i class="fab fa-medium"></i>
+							</span>
+
+							@if ($errors->has('medium_url'))
+								<span class="help is-danger">
+									{{ $errors->first('medium_url') }}
+								</span>
+							@endif
+						</div>
+					</div>
+
+
+					<div class="field">
+						<label class="label" for="twitter_url">
+							Twitter
+						</label>
+						<div class="control has-icons-left">
+
+							{{ Form::url('twitter_url', $user->twitter_url, ['class' => 'input', 'placeholder' => 'https://twitter.com']) }}
+
+							<span class="icon is-small is-left">
+								<i class="fab fa-twitter"></i>
+							</span>
+
+							@if ($errors->has('twitter_url'))
+								<span class="help is-danger">
+									{{ $errors->first('twitter_url') }}
+								</span>
+							@endif
+						</div>
+					</div>
+
+
+					<div class="field">
+						<label class="label" for="youtube_url">
+							YouTube
+						</label>
+						<div class="control has-icons-left">
+
+							{{ Form::url('youtube_url', $user->youtube_url, ['class' => 'input', 'placeholder' => 'https://youtube.com']) }}
+
+							<span class="icon is-small is-left">
+								<i class="fab fa-youtube"></i>
+							</span>
+
+							@if ($errors->has('youtube_url'))
+								<span class="help is-danger">
+									{{ $errors->first('youtube_url') }}
+								</span>
+							@endif
+						</div>
+					</div>
+
+
+					<div class="field">
+						<label class="label" for="linkedin_url">
+							LinkedIn
+						</label>
+						<div class="control has-icons-left">
+
+							{{ Form::url('linkedin_url', $user->linkedin_url, ['class' => 'input', 'placeholder' => 'https://linkedin.com']) }}
+
+							<span class="icon is-small is-left">
+								<i class="fab fa-linkedin"></i>
+							</span>
+
+							@if ($errors->has('linkedin_url'))
+								<span class="help is-danger">
+									{{ $errors->first('linkedin_url') }}
+								</span>
+							@endif
+						</div>
+					</div>
+
+
+				</div>
+			</article>
+
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+
 
 			<p>&nbsp;</p>
 			<p>&nbsp;</p>
