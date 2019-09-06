9044a14cf00c162771d4778888ac0277f56a3dd4
Hillel Coren <hillelcoren@gmail.com>
2019-09-06 14:56:39 +0300

    Profiles

--- resources/views/flutter_pro/index.blade.php
+++ resources/views/flutter_pro/index.blade.php
@@ -1,7 +1,7 @@
 @extends('master')
 
 @section('title', 'A Showcase for Passionate Flutter Developers')
-@section('description', 'A Showcase for Passionate Flutter Developers')
+@section('description', 'Profiles are sourced from FlutterX, Flutter Events and It\'s All Widgets!')
 @section('image_url', asset('images/flutterpro_twitter.png'))
 
 @section('header_title', 'A Showcase for Passionate Flutter Developers')
