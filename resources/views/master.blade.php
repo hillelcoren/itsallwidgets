6ddec9506484010e8a8a0b39697e957ed5c451dd
Hillel Coren <hillelcoren@gmail.com>
2019-09-23 00:09:11 +0300

    Update footer

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -566,15 +566,15 @@
                         </div><br/>
                     @elseif (isFP())
                         <div class="column is-1">
-                            <a href="https://twitter.com/lariki" target="_blank">
-                                <img src="{{ asset('images/img_lara.png') }}" width="72"/><br/>
-                                <div>Lara Martín</div>
-                            </a>
-                        </div><br/>
-                        <div class="column is-1">
                             <a href="https://twitter.com/imthepk" target="_blank">
                                 <img src="{{ asset('images/img_pawan.png') }}" width="72"/><br/>
                                 <div>Pawan Kumar</div>
+                            </a>
+                        </div><br/>
+                        <div class="column is-1">
+                            <a href="https://twitter.com/lariki" target="_blank">
+                                <img src="{{ asset('images/img_lara.png') }}" width="72"/><br/>
+                                <div>Lara Martín</div>
                             </a>
                         </div><br/>
                         <div class="column is-1">
