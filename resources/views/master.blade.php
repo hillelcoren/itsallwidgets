5043492a8f4f86346ed182b0845473578ef62ab8
Hillel Coren <hillelcoren@gmail.com>
2020-06-01 17:21:47 +0300

    Flutter Streams

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -483,7 +483,7 @@
 
                     @if (isIAW())
                         &nbsp;
-                        <a style="padding: 20px 32px 18px 32px; color:white; background-color: #366cb6; border-color:#366cb6;"
+                        <a style="padding: 20px 32px 18px 32px; color:white; background-color: #366cb6; border-color:#366cb6;" target="_blank"
                             class="button is-elevated-dark" href="@yield('second_header_button_url', 'https://twitter.com/itsallwidgets')">
                             <span class="icon">
                                 <i class="@yield('second_header_button_icon', 'fab fa-twitter')"></i>
