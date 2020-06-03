fe3ea542f60c9dded6b42674f420eeff83a026a4
Hillel Coren <hillelcoren@gmail.com>
2020-06-03 15:55:05 +0300

    Streams

--- resources/views/flutter_streams/index.blade.php
+++ resources/views/flutter_streams/index.blade.php
@@ -202,12 +202,11 @@
                                                 <div style="color:#888; font-size:15px; padding-top: 10px;" class="channel-name">
                                                     @{{ stream.time_ago }}
 
+                                                    <!--
                                                     <SPAN v-if="stream.is_past">P </SPAN>
                                                     <SPAN v-if="stream.is_past_sub1">1 </SPAN>
                                                     <SPAN v-if="stream.is_past_sub2">2 </SPAN>
                                                     <SPAN v-if="stream.is_past_sub3">3 </SPAN>
-
-                                                    <!--
                                                     <SPAN v-if="stream.is_soon">S </SPAN>
                                                     <SPAN v-if="stream.is_live">L </SPAN>
                                                     -->
