702d8854f41334e63c8ec7278ebae989ae90ce71
Hillel Coren <hillelcoren@gmail.com>
2021-02-17 17:17:45 +0200

    Show badge message on edit app page

--- resources/views/flutter_pro/cards.blade.php
+++ resources/views/flutter_pro/cards.blade.php
@@ -9,8 +9,8 @@
 
 @foreach ($users as $user)
     <tr>
-        <td>{{ $user->widget_youtube_url ? '<a href="https://www.youtube.com/watch?v=' . $user->widget_youtube_url . '" target="_blank">' . $user->widget . '</a>' : $user->widget }}</td>
-        <td>{{ $user->twitter_url ? '<a href="' . $user->twitter_url . '" target="_blank">' . $user->twitterHandle() . '</a>' : '' }}</td>
+        <td><a href="https://www.youtube.com/watch?v={{ $user->widget_youtube_url }}" target="_blank">{{ $user->widget }}</a></td>
+        <td><a href="{{ $user->twitter_url }}" target="_blank">{{ $user->twitterHandle() }}</a>
         <td>{{ $user->widget_description }} [{{ strlen($user->widget_description) }}]</td>
         <td>{{ $user->widget_tip }} [{{ strlen($user->widget_tip) }}]</td>
         <td><pre>{{ $user->widget_code_sample }}</pre> [{{ strlen($user->widget_code_sample) }}]</td>
