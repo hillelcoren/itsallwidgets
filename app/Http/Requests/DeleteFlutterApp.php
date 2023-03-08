ea93fdd03f5e03ae0a02e26e2352e1ca23f18714
Hillel Coren <hillelcoren@gmail.com>
2023-03-08 15:31:35 +0200

    Enable users to delete their app

---
+++ app/Http/Requests/DeleteFlutterApp.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DeleteFlutterApp extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->check() && auth()->user()->id == $this->flutter_app->user_id;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [];
+    }
+}
