8912ea4d82b0612422cedd593b0a4b8ad7e4d630
Hillel Coren <hillelcoren@gmail.com>
2023-09-06 13:46:23 +0300

    Try to fix tweets

--- app/Console/Commands/TweetStreams.php
+++ app/Console/Commands/TweetStreams.php
@@ -62,10 +62,12 @@ class TweetStreams extends Command
                     ->get();
 
         foreach ($streams as $stream) {
+            /*
             $response = $twitter->upload('media/upload', [
                 'media' => $stream->getThumbnailUrl(),
                 'media_type' => 'image/jpg'
             ]);
+            */
 
             $startsAtDate = Carbon::parse($stream->starts_at);
             $tweet = '📢 ' . $stream->channel->name . "'s";
@@ -92,7 +94,7 @@ class TweetStreams extends Command
 
             $parameters = [
                 'text' => $tweet,
-                'media_ids' => $response->media_id_string
+                //'media_ids' => $response->media_id_string
             ];
 
             $twitter->setApiVersion('2');
