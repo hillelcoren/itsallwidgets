7aa483b6f50d95282ef1a2dd3efba1ff1ea89369
Hillel Coren <hillelcoren@gmail.com>
2024-11-03 22:41:34 +0200

    Add frame header

--- app/Http/Middleware/FrameHeaders.php
+++ app/Http/Middleware/FrameHeaders.php
@@ -9,7 +9,8 @@ class FrameHeaders
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        $response->headers->set('X-Frame-Options', 'https://flutterpro.dev');
+        //$response->headers->set('X-Frame-Options', 'ALLOW-FROM https://flutterpro.dev');
+        $response->headers->set('Content-Security-Policy', "frame-ancestors 'self' https://flutterpro.dev");
         return $response;
     }
 }
