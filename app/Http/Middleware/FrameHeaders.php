43ee3546f41b98b776f426548d75b166ba4b6101
Hillel Coren <hillelcoren@gmail.com>
2024-11-03 22:38:59 +0200

    Add frame header

---
+++ app/Http/Middleware/FrameHeaders.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class FrameHeaders
+{
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+        $response->headers->set('X-Frame-Options', 'https://flutterpro.dev');
+        return $response;
+    }
+}
